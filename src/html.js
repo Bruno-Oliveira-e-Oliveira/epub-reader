0b7ef9bba78dbf1ca7c529d1fcf9d356e8e33c83
BrunoOliveira <brunooliveiraeoliveira@outlook.com>
2022-08-07 11:02:20 -0300

    Refactoring the getTag method and your dependencies and creating the getTagAttributes method

--- src/html.js
+++ src/html.js
@@ -6,63 +6,93 @@ class Html {
     read() {
         let baseContent = this.baseContent;
 
-        baseContent = this.getTag(baseContent);
-        // baseContent = this.getTag(baseContent);
-
+        //--- Test    
+        for (let index = 0; index < 2; index++) {
+            baseContent = this.getTag(baseContent);
+        }
 
     }
 
     getTag(text) {
+        let tag  = {};
         let start = text.search('<');
         let end = text.search('>') + 1;
-        let tag = text.substring(start,end);
+        let originalContent = text.substring(start,end);
 
-        // if (this.isXml(tag)){
-            
-        // }
+        let croppedContent = this.removeTagSymbols(originalContent, start, end);
+        tag.name = this.getTagName(croppedContent);
+        let namePosition = croppedContent.search(tag.name) + tag.name.length;
+        croppedContent = croppedContent.substring(namePosition);
+        croppedContent = croppedContent.trim();
 
-        this.getTagElements(start, end, tag)
+        tag.attributes = this.getTagAttributes(croppedContent);
+        tag.inner = []; //--- TODO
 
+        console.log(originalContent);
         console.log(tag);
 
         return text.substring(end);
     }
 
-    getTextPosition(text, searchText) {
-        let position = text.search(searchText);
-        return position;
-    }
-
-    isXml(tag) {
-        let begin = tag.search('<?');
-        let xml = tag.search('xml');
-        let end = tag.search('/?>');
-
-        if (begin >= 0 && xml >= 0 && end >= 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    getTagElements(start, end, tag) {
-        let tagCropped = this.removeTagSymbols(start, end, tag);
-        let tagName = this.getTagName(tagCropped);
-
-
-        console.log(tagName)
-    }
-
-    removeTagSymbols(start, end, tag) {
-        let tagCropped = tag.substring(0, end -1);
-        tagCropped = tagCropped.substring(start +1);
-        return tagCropped.trim();
+    removeTagSymbols(tag, start, end) {
+        let croppedContent = tag.substring(0, end -1);
+        croppedContent = croppedContent.substring(start +1);
+        return croppedContent.trim();
     }
 
     getTagName(tag){
         let firstSpace = tag.search(' ');
-        return tag.substring(0, firstSpace);
+        let tagName = tag.substring(0, firstSpace);
+        tagName = tagName.replace(/[^a-zA-Z0-9 ]/g, '');
+        return tagName;
+    }    
+
+    getTagAttributes(croppedContent) {
+        const attributes = [];
+        
+        let continueLoop = true;
+        do {
+            let attribute = {};
+            let firstSpace = croppedContent.search(' ');
+
+            if (firstSpace >= 0) {
+                attribute = this.divideAttributeNameAndValue(croppedContent.substring(0, firstSpace));
+                croppedContent = croppedContent.substring(firstSpace);
+                croppedContent = croppedContent.trim();     
+                attributes.push(attribute);
+            } else {
+                if (croppedContent.length > 0) {               
+                    attribute = this.divideAttributeNameAndValue(croppedContent);
+                    if (attribute) {
+                        attributes.push(attribute);
+                    }
+                }
+                continueLoop = false; 
+            }
+
+        } while (continueLoop);
+
+        return attributes;
     }
+
+    divideAttributeNameAndValue(croppedContent) {
+        let attribute = {};
+        let equalsPosition = croppedContent.search('=');
+
+        if (equalsPosition >= 0) {
+            attribute.name = croppedContent.substring(0, equalsPosition);
+            croppedContent = croppedContent.substring(equalsPosition + 1);
+            let firstChar = croppedContent.substring(0,1);
+            croppedContent = croppedContent.substring(1);
+            let endValuePosition = croppedContent.search(firstChar);
+            attribute.value = croppedContent.substring(0, endValuePosition);
+            return attribute;
+        } else {
+            return undefined; //--- attributes without value?
+        }
+    }
+
+
 
 }
 
